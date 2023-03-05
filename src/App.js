8d5b3d021316aaa265d94df88ef045f5a610a69c
Saurabh <saurabkumar679@gmail..com>
2023-03-05 13:48:32 +0530

    react router configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import {Routes, Route, Navigate} from "react-router-dom";
+import { Home,About,Contact } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      Sanyam sood 
-    </div>
+    <Routes>
+      <Route  path='/' element={<Home/>}/>
+      <Route  path='/about' element={<About/>}/>
+      <Route  path='/contact' element={<Contact/>}/>
+      <Route  path='*' element={<Navigate to='/'/>}/>
+    </Routes>
   );
 }
 
